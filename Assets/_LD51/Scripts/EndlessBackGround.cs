1e776e5f5b76e89b78ca7e69a2a22c637080fa68
gildar76 <gildar76@gmail.com>
2022-10-01 15:11:18 +0200

    Add endless background

---
+++ Assets/_LD51/Scripts/EndlessBackGround.cs
@@ -0,0 +1,66 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace GildarGaming.LD51
+{
+    public class EndlessBackGround : MonoBehaviour
+    {
+        [SerializeField] Transform[] backgroundTransform;
+        [SerializeField] Transform cameraTransform;
+        BoxCollider2D col;
+        public bool isVisible;
+        private void Start()
+        {
+            cameraTransform = Camera.main.transform;
+            col = GetComponent<BoxCollider2D>();
+            //MoveBackground();
+        }
+        private void MoveBackground(int direction = 1)
+        {
+            if (!this.isVisible)
+            {
+
+                foreach (Transform t in backgroundTransform)
+                {
+
+                    if (t == this.transform) continue;
+                    print(t.gameObject.name);
+                    
+                    
+                        
+                        float newX = t.transform.position.x + (col.size.x * direction);
+                        transform.position = new Vector3(newX, transform.position.y, transform.position.z);
+                        break;
+                    
+
+                }
+            }
+        }
+
+
+        private void LateUpdate()
+        {
+
+            if (this.transform.position.x < cameraTransform.position.x - 40 && isVisible)
+            {
+                isVisible = false;
+                MoveBackground();
+
+            } else if (this.transform.position.x > cameraTransform.position.x + 40 && isVisible) 
+            {
+                
+                isVisible = false;
+                MoveBackground(-1);
+                print("inside else if");
+            }
+            else
+            {
+                isVisible = true;
+            }
+            
+            
+        }
+
+    }
+}
